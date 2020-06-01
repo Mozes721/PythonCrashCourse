be335a65881887b7d9d872cca630fe27f43b4c8d
eric <eric@mail.com>
2020-06-01 15:43:18 +0200

    finished
    "Done"

--- chapter_18/pizza_project/Pizza/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from Pizza.models import Pizza, Toppings
-# Register your models here.
-admin.site.register(Pizza)
-admin.site.register(Toppings)
--- chapter_18/pizza_project/l_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/richard/Desktop/PythonCrashCourse/chapter_18/pizza_project/l_env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
